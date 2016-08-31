d9f5d2d3041991d28a389f398bf551f9ea6adff9
Jason <jsagisi@csumb.edu>
2016-08-31 10:44:56 -0700

    Added a comment

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from PIL import Image
 myImage = [0] * 9
 #myImage = Image.open("Project1Images/1.png")
 #myImage.show()
+#This is a for loop
 for i in range (1,9):
 	myImage[i]=Image.open("Project1Images/%d.png" %i)
 	myImage[i].show()
+	
+
 #height = image[1,9].height
